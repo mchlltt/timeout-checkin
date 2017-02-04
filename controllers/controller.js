57856abaf6af6dce80f6549658ad2da7ebc97e1c
Mich Elliott <me@northwestern.edu>
2017-02-04 10:30:43 -0600

    Remove burger references.

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,42 +5,9 @@ var router = express.Router();
 // Import data models.
 var db = require('../models');
 
-// GET route which calls the data model's 'all' method.
-// This route then hands the data it receives to handlebars so index can be rendered.
+// GET route which returns the index.
 router.get('/', function (req, res) {
-    db.burgers.findAll({
-        order: 'burger_name ASC'
-    }).then(function (data) {
-        var hbsObject = {
-            burgers: data
-        };
-        res.render('index', hbsObject);
-    });
-});
-
-
-// POST route which calls the model's 'post' method with the burger name given.
-router.post('/', function (req, res) {
-    var burgerName = req.body.name;
-    db.burgers.create({
-        burger_name: burgerName
-    }).then(function () {
-        res.redirect('/');
-    });
-});
-
-// PUT (update) route which calls the model's update method.
-// This route sends the id and 'devoured' state on the burger modified.
-router.put('/:id', function (req, res) {
-    var devoured = req.body.devoured;
-    var ID = req.params.id;
-
-    db.burgers.update(
-        {devoured: devoured},
-        {where: {id: ID}}
-    ).then(function () {
-        res.redirect('/');
-    });
+    res.render('index');
 });
 
 // Export routes for server.js.
