0fd1190df1c6169aa7b674babe7ae10927c17e4b
Matthew Tutak <mtutak91@gmail.com>
2017-02-04 12:51:48 -0600

    changed to CDNs added slickjs

--- public/assets/js/design.js
+++ public/assets/js/design.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
     $('select').material_select();
     $('.button-collapse').sideNav();
-    $('.carousel.carousel-slider').carousel({
-        fullWidth: true
-    });
+    // $('.carousel.carousel-slider').carousel({
+    //     fullWidth: true
+    // });
     $('#start').on('click', show);
 
     function show() {
         $('#index-banner').fadeOut('slow');
     }
+    $('.single-item').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        speed: 300,
+        accessibility: true
+    });
 });
