1198b7f563682739804430043f92b752b4d9ddd3
Matthew Tutak <mtutak91@gmail.com>
2017-02-06 19:58:30 -0600

    slider

--- public/assets/js/slider.js
+++ public/assets/js/slider.js
@@ -1,29 +1,19 @@
-$('#start-button').on('click', function() {
+$('#start-button').on('click', function () {
     $('.questions').slick('slickNext');
 });
-
-$('#super-category-submit').on('click', function() {
+$('#super-category-submit').on('click', function () {
     var selection = $('input:radio[name=group1]:checked').data('name');
-    $.get('/api/feeling-categories/' + selection).done(
-        function(data) {
-
-            // Use the data to generate the next set of options.
-            console.log(data);
-            $('.questions').slick('slickNext');
-        }
-    );
-
+    $.get('/api/feeling-categories/' + selection).done(function (data) {
+        // Use the data to generate the next set of options.
+        console.log(data);
+        $('.questions').slick('slickNext');
+    });
 });
-
-$('#category-submit').on('click', function() {
+$('#category-submit').on('click', function () {
     var selection = $('input:radio[name=group2]:checked').data('name');
-    $.get('/api/feelings/' + selection).done(
-        function(data) {
-
-            // Use the data to generate the next set of options.
-            console.log(data);
-            $('.questions').slick('slickNext');
-        }
-    );
-
+    $.get('/api/feelings/' + selection).done(function (data) {
+        // Use the data to generate the next set of options.
+        console.log(data);
+        $('.questions').slick('slickNext');
+    });
 });
