670448e092a9fbb22271143872db59744170f5f6
Matthew Tutak <mtutak91@gmail.com>
2017-02-11 10:32:12 -0700

    fixing client-side ajax call

--- public/assets/js/slider.js
+++ public/assets/js/slider.js
@@ -1,63 +1,58 @@
-$('#start-button').on('click', function() {
+$('#start-button').on('click', function () {
     $('.questions').slick('slickNext');
 });
-
-$('.back-button').on('click', function() {
+$('.back-button').on('click', function () {
     $('.questions').slick('slickPrev');
 });
-
-$('#super-category-submit').on('click', function() {
+$('#super-category-submit').on('click', function () {
     var selection = $('input:radio[name=super-category]:checked').val();
     if (selection === undefined) {
         return;
     }
-    $.get('/api/feeling-categories/' + selection).done(function(data) {
+    $.get('/api/feeling-categories/' + selection).done(function (data) {
         $('#categories-options').empty();
         // Use the data to generate the next set of options.
-        data.forEach(function(datum) {
+        data.forEach(function (datum) {
             $('#categories-options').append('<input name="category" type="radio" id="category-' + datum.id + '" value="' + datum.id + '" />').append('<label for="category-' + datum.id + '" class="white-text">' + datum.name + '</label>')
         });
         $('.questions').slick('slickNext');
     });
 });
-$('#category-submit').on('click', function() {
+$('#category-submit').on('click', function () {
     var selection = $('input:radio[name=category]:checked').val();
     if (selection === undefined) {
         return;
     }
-    $.get('/api/feelings/' + selection).done(function(data) {
+    $.get('/api/feelings/' + selection).done(function (data) {
         $('#feelings-options').empty();
         // Use the data to generate the next set of options.
-        data.forEach(function(datum) {
+        data.forEach(function (datum) {
             $('#feelings-options').append('<input name="feeling" type="radio" id="feeling-' + datum.id + '" value="' + datum.id + '" />').append('<label for="feeling-' + datum.id + '" class="white-text">' + datum.name + '</label>');
         });
         $('.questions').slick('slickNext');
     });
 });
-$('#feeling-submit').on('click', function() {
+$('#feeling-submit').on('click', function () {
     var selection = $('input:radio[name=feeling]:checked').val();
     if (selection === undefined) {
         return;
     }
-    $.get('/api/resource-category-popularity/' + selection).done(function(data) {
-        Object.keys(data).forEach(function(key) {
+    $.get('/api/resource-category-popularity/' + selection).done(function (data) {
+        Object.keys(data).forEach(function (key) {
             $('#percentage-' + key).empty().append(' ' + Math.round(data[key] * 100) + '%');
         });
         $('.questions').slick('slickNext');
     });
 });
-
-$('#resource-category-submit').on('click', function() {
+$('#resource-category-submit').on('click', function () {
     // Get resource category selection.
     var selection = $('input:radio[name=resource-category]:checked').val();
     // Get feeling ID from previous page.
     var FeelingId = $('input:radio[name=feeling]:checked').val();
-
     // Must select value
     if (selection === undefined) {
         return;
     }
-
     if (selection && FeelingId) {
         // Post resource ID and feeling ID.
         $.post('/api/new', {
@@ -65,26 +60,29 @@ $('#resource-category-submit').on('click', function() {
             ResourceCategoryId: selection
         });
     }
-
-    // // AJAX request for data
-    // $.get('/api/resources/' + selection, function(data) {
-
-    //     // Get the length of the data returned.
-    //     var numberResults = data.length;
-    //     var whichResult = Math.floor(Math.random() * numberResults);
-
-    //     var resource = data[whichResult];
-    //     ResourceId = resource.id;
-
-    //     if (resource.embed) {
-    //         $('#resource-title').empty().append('<h1><a href="' + resource.content + '" class="white-text" target="_blank">' + resource.name + '</a></h1>');
-    //         $('#resource-content').empty().append('<iframe width="400px" height="300px" src="' + resource.embed + '"></iframe>');
-    //     } else {
-    //         $('#resource-title').empty();
-    //         $('#resource-content').empty().append('<h1><a href="' + resource.content + '" class="white-text" target="_blank">' + resource.name + '</a></h1>');
-    //     }
-
-    //     $('.questions').slick('slickNext');
-
-    // });
+    //Ajax request for data
+    $.get('/api/resources/' + selection, function (data) {
+        console.log(data);
+        if (selection === '3') {
+            $('#resTitle').html('<h1 class="resMain">' + data.name + '</h1>');
+            $('#resContent').html('<h1 class="resQuote">' + data.content + '</h1>');
+        } else if (selection === '1') {
+            $('#resTitle').html('<h1 class="resMain">' + data.name + '</h1>');
+            $('#resContent').html('<div class="videoWrapper"><iframe id="video"  src = "https://www.youtube.com/embed/' + data.content + '?autoplay=1&rel=0&showinfo=0&color=white&autohide=0" > < /iframe></div>');
+            //iframe width="560" height="315"
+        } else if (selection === '4') {
+            console.log(data);
+            //Could Add Image Thumbnail
+            // var img = data.media[1];
+            // console.log(img);
+            // <img src="' + img + '"></img>
+            $('#resTitle').html('<h1 class="news resMain">' + data.title + '</h1><p class="news abstract">' + data.abstract + '</p><h3 class="news url">' + data.url + '</h3>');
+        } else if (selection === '2') {
+            $('#resTitle').html('<h1 class="resMain">' + data.name + '</h1>');
+            $('#resContent').html('<h1 class="resQuote">' + data.content + '</h1>');
+        } else {
+            $('#resources').append('<h2>' + data.name + '</h2><h3>' + data.content + '</h3>');
+        }
+        $('.questions').slick('slickNext');
+    });
 });
